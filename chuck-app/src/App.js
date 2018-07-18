96de9f63ce1f579262df950cb5817dba103f075d
Marie Gimer <marie-gimer14@hotmail.fr>
2018-07-18 21:06:06 +0200

    affichage data chuck

--- chuck-app/src/App.js
+++ chuck-app/src/App.js
@@ -2,17 +2,16 @@ import React, { Component } from 'react';
 import './App.css';
 import SearchContainer from './components/SearchContainer';
 
-import gif from '../public/img/chuck-gif.gif' // relative path to image
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <div className="App-header">
-          <h1>Chuck Norris</h1>
-            <img src={gif} alt={""} />
+          <h1>Chuck Norris Search</h1>
         </div>
-      <div class="container">
+      <div className="container">
           <SearchContainer />
       </div>
       </div>
--- chuck-app/src/components/SearchContainer.js
+++ chuck-app/src/components/SearchContainer.js
@@ -1,95 +1,43 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-import update from 'immutability-helper';
+import '../App.css';
+import Results from "./Results";
 
-class SearchContainer extends Component{
 
-    constructor(props){
-        super(props);
-        this.state = {
-            ideas: [],
-            editingIdeaId : null,
-            notification: ''
-        }
+class SearchContainer extends Component {
+
+
+    constructor() {
+        super();
+        this.state = {username: []};
+
+        this.handleClick = this.handleClick.bind(this)
     }
 
-
-    componentDidMount(){
-
-        axios.get('http://localhost:3001/api/v1/ideas.json')
-            .then( response => {
-                console.log(response);
-                this.setState({ideas : response.data})
+    handleClick() {
+        axios.get('https://api.chucknorris.io/jokes/random')
+            .then((response) => {
+                this.setState({username: response.data.value})
             })
-            .catch(error => console.log(error))
     };
 
-    addNewIdea = () => {
-        axios.post('http://localhost:3001/api/v1/ideas',
-            {idea: {
-                title: " ", body: " " }
-            })
-            .then(response => {
-                console.log(response);
-                const ideas = update(this.state.ideas, {$splice: [[0, 0, response.data]]});
-                this.setState({ideas: ideas, editingIdeaId: response.data.id});
-            })
-            .catch(error => console.log(error));
 
-    };
-
-    updateIdea = (idea) => {
-        const ideaIndex = this.state.ideas.findIndex(x => x.id === idea.id);
-        const ideas = update(this.state.ideas, {[ideaIndex]: {$set: idea}});
-        this.setState({ideas: ideas, notification: "All changes saved"});
-
-    };
-
-    resetNotification = () => {
-        this.setState({notification: ''})
-    };
-
-    enableEditing = (id) => {
-        this.setState({editingIdeaId: id},
-            () => { this.title.focus() })
-    };
-
-    deleteIdea = (id) => {
-        axios.delete(`http://localhost:3001/api/v1/ideas/${id}`)
-            .then(response => {
-                const ideaIndex = this.state.ideas.findIndex(x => x.id === id)
-                const ideas = update(this.state.ideas, { $splice: [[ideaIndex, 1]]})
-                this.setState({ideas: ideas})
-            })
-            .catch(error => console.log(error))
-    };
-
-    render(){
-        return(
+    render() {
+        return (
             <div>
-                <div class="search-container">
+                <div className="search-container">
                     <form action="#">
-                        <input type="text" placeholder="Search.." name="search" />
-                        <button type="submit">Rechercher</button>
-                    </form>
-                </div>
-                <div>
-                 {/*{this.state.ideas.map((idea) =>{
-                    if (this.state.editingIdeaId === idea.id){
-                    // {
-                    //     return <IdeaForm idea={idea} key={idea.id} updateIdea={this.updateIdea}
-                    //                      resetNotification={this.resetNotification} titleRef= {input => this.title = input}/>
-                    // }
-                    // else {
-                    //     return <Idea idea={idea} key={idea.id} onClick={this.enableEditing} onDelete={this.deleteIdea}/>
-                    // }
-                })}*/}
+                        <input type="text" placeholder="Search.." onChange={this.searchBar} name="search" />
+                        <button onClick={this.handleClick()}>Rechercher</button>
+                        </form>
                 </div>
 
+                    <p>{this.state.username}</p>
+                <Results/>
             </div>
-
         )
-    };
+    }
+
 }
 
 export default SearchContainer;
